4db9fb8444b653fb02839744fdd27cf543630382
Juan J. Torres <juanjtp93@gmail.com>
2015-12-07 13:32:17 -0700

    Nuevos archivos

---
+++ system/login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Govir</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+    <link href="css/mystyles.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+     <h2>Govir</h2>
+    <section class="container login-form">
+        <form method="post" action="" role="login">
+            <section>
+                <h4 class="text-center">LOGIN</h4>
+                <img src="assets/images/EscudoUson.png" width="100px" class="img-responsive">
+                
+                
+        
+                <div class="form-group">
+                    <H2>Nombre de usuario</H2>
+                    <input type="text" name="user" placeholder="Nombre de usuario" required class="form-control input-lg" />
+                </div>
+                <div class="form-group">
+                    <h2>Contraseña</h2>
+                    <input type="password" name="password" placeholder="Contraseña" required class="form-control input-lg" />
+                </div>
+                
+                <!--<a href="#">¿Olvidaste tu contraseña?</a>-->
+                <br>
+                
+                <button type="submit" name="submit" class="btn btn-lg btn-block btn-success">Iniciar Sesión</button>
+            </section>
+        </form>
+
+        
+    </section>
+   
+  
+</body>
+
+</html>
